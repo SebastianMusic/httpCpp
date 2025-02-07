7a3d33030d881ab10d06cfa2635ae0eb03b4fa19
sebastianmusic <sebastian@jsst.no>
2025-02-07 14:25:11 +0100

    Added ability to send html files

--- main.cpp
+++ main.cpp
@@ -1,11 +1,56 @@
 #include <cstddef>
 #include <cstdio>
 #include <cstring>
+#include <fcntl.h>
 #include <iostream>
 #include <iterator>
 #include <netinet/in.h>
+#include <ostream>
+#include <string>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <unistd.h>
+
+void sendHtml(int fd, std::string path) {
+  std::string httpHeader = "HTTP/1.1 200 OK\r\n"
+                           "Content-Length: CONTENTLENGTHREPLACE\r\n"
+                           "Content-Type: text/html\r\n"
+                           "Connection: close\r\n"
+                           "\r\n";
+  // calculate the size of the html file
+  char sendBuffer[1000];
+  struct stat statBuffer{};
+
+  int htmlFd =
+      open(path.c_str(), O_RDONLY, 0); // convert std::string to c style string
+  if (htmlFd < 0) {
+    perror("open failed in sendhtml");
+    std::cout << path.c_str();
+  }
+  if (fstat(htmlFd, &statBuffer) < 0) {
+    perror("fstat failed in sendHtml()");
+  }
+  size_t fileSize = statBuffer.st_size;
+  std::string fileSizeString = std::to_string(fileSize);
+
+  size_t pos = httpHeader.find("CONTENTLENGTHREPLACE");
+
+  if (pos != std::string::npos) {
+    httpHeader.replace(pos, std::strlen("CONTENTLENGTHREPLACE"),
+                       fileSizeString);
+  }
+  size_t httpHeaderLen = httpHeader.length();
+  memcpy(sendBuffer, httpHeader.c_str(), httpHeaderLen);
+
+  if (read(htmlFd, sendBuffer + httpHeaderLen, fileSize) < 0) {
+    perror("sendHtml failed in read");
+  }
+  if (write(fd, sendBuffer, httpHeaderLen + fileSize) < 0) {
+    perror("sendHtml failed in write");
+
+    std::cout << sendBuffer;
+  }
+}
 
 struct clientMessage {
   char msg[100];
@@ -51,8 +96,6 @@ int main() {
 
   int clientSocket = accept(serverSocket, 0, 0);
 
-  write(1, &msg, totalLen);
-  write(clientSocket, &msg, totalLen);
-
+  sendHtml(clientSocket, "/home/sebastian/kode/cpp/index.html\0");
   return 0;
 }
