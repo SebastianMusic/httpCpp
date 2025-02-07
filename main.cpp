a5cb395c110c0fad98480ab14bcbed3c6539f0b2
sebastianmusic <sebastian@jsst.no>
2025-02-07 13:29:38 +0100

    added first attempt at sending over data

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
+#include <cstddef>
 #include <cstdio>
+#include <cstring>
 #include <iostream>
+#include <iterator>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <unistd.h>
+
+struct clientMessage {
+  char header[100];
+  char content[5];
+};
 
 int main() {
+  clientMessage msg;
+
+  const char *httpHeader = "HTTP/1.1 200 OK\r\n"
+                           "Content-Length: 5\r\n"
+                           "Content-Type: text/plain\r\n"
+                           "Connection: close\r\n\r\n";
+
+  const char *httpContent = "halla";
+
+  size_t httpHeaderLen = strlen(httpHeader);
+  size_t httpContentLen = strlen(httpContent);
+
+  memcpy(msg.header, httpHeader,
+         httpHeaderLen); // copies only specified memory no extra nullbyte
+                         // padding like strncpy
+  memcpy(msg.content, httpContent, httpContentLen);
+
   struct sockaddr_in serverAddr{};
   serverAddr.sin_family = AF_INET;
   serverAddr.sin_port = htons(8080);
@@ -17,6 +43,14 @@ int main() {
     perror("bind failed");
     return 1;
   }
+
+  if (listen(serverSocket, 128) < 0) {
+    perror("listen failed");
+  }
+
+  int clientSocket = accept(serverSocket, 0, 0);
+
+  write(clientSocket, &msg, sizeof(msg));
 
   return 0;
 }
